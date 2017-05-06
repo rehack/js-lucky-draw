e0fd7ede9e8ae2d8db5a546abd0edf18f4535372
codesboy <565195693@qq.com>
2017-05-06 18:32:29 +0800

    update

--- js/luck2.js
+++ js/luck2.js
@@ -1,52 +1,79 @@
 class Lucky{
-    constructor(startNum,endNum,digits,numWrap){
+    constructor(startNum,endNum,numPwrap,totalTurns){
         this.startNum=startNum;//参与抽奖的起始号码
         this.endNum=endNum;//参与抽奖的结束号码
-        this.digits=digits;//中奖号码显示的位数
+        this.digits=this.endNum.toString().length;//中奖号码显示的位数
         this.allList=[];//参与抽奖号码数组
+        this.totalTurns=totalTurns;//总抽奖轮次
         this.luckyNum=null;//中奖号码
         this.flag=true;//开始与停止标记
-        this.numPwrap=document.getElementById(numWrap);//号码显示父容器
-        this.numWrap=this.numPwrap.getElementsByTagName('span');//号码显示父容器
+        this.numPwrap=document.getElementById(numPwrap);//号码显示父容器
+        this.turnsWrap=document.getElementById('turns');//显示轮次容器
+        this.numWrap=this.numPwrap.getElementsByTagName('span');//子节点个数
         this.timer=null;//定时器
         this.oLocalStorage=window.localStorage;//本地存储对象
-        this.turn=1;//97->1 98->2 ... 抽奖轮次编号 按小键盘数字进行切换
+        this.turn=1;//97->1 98->2 ... 抽奖轮次编号 按左右方向键进行切换
         // this._d=this.d();
+        this.d=0;//中奖号码滚动位置
     }
 
     init(){
-        this.getLocalStorage(this.turn);//刷新页面，从localStoreage中恢复
+        // 得到参与抽奖号码数组
+        for(let i=this.startNum;i<=this.endNum;i++){
+            this.allList.push(i);
+        }
+
+
+        //刷新页面，从localStoreage中恢复
+        this.fill();
         // console.log(this.getLocalStorage())
         document.onkeyup=(e)=>{
             let oEvent=e||window.event;//let在当前作用域内有用，未声明前不可用，不可重复声明
 
 
             // alert(oEvent.keyCode)
-            // console.log(this)
+
             // 空格键开始和停止抽奖
-
-
             if(oEvent.keyCode==32 && this.flag){
                 // console.log(oEvent.keyCode);
                 this.flag=false;
                 this.run();
+                console.log('开始')
             }else if (oEvent.keyCode==32 && !this.flag) {
                 this.flag=true;
                 this.stop();
+                 console.log('停止')
+            }
+
+            // 左右切换抽奖轮次
+            if(oEvent.keyCode==37){
+                // 左切换
+                this.turn-=1;
+                if(this.turn==0){
+                    this.turn=this.totalTurns;
+                }
+                this.fill();
+            }else if(oEvent.keyCode==39){
+                // 右切换
+                this.turn+=1;
+                if(this.turn>this.totalTurns){
+                    this.turn=1;
+                }
+                this.fill();
             }
 
             switch (oEvent.keyCode) {
                 case 97:
                     this.turn=1;
-                    this.getLocalStorage(this.turn);
+                    this.fill();
                     break;
                 case 98:
                     this.turn=2;
-                    this.getLocalStorage(this.turn);
+                    this.fill();
                     break;
                 case 99:
                     this.turn=3;
-                    this.getLocalStorage(this.turn);
+                    this.fill();
                     break;
                 case 27:
                     this.oLocalStorage.clear();
@@ -74,79 +101,96 @@ class Lucky{
      * @return {[number]} [带前导0的数字]
      */
     getLucky(){
-        for(let i=this.startNum;i<=this.endNum;i++){
-            this.allList.push(i);
-        }
-        // return this.allList;
         let randomNum=Math.floor(Math.random() * this.allList.length);//随机抽取一个中奖号码
-        // return this.buquan(this.allList[randomNum],this.digits);//将中奖号码补全前导0 然后返回
         return this.buquan(this.allList[randomNum],this.digits);//将中奖号码补全前导0 然后返回
-        // return this.luckyNum;
     }
 
-    d(){
-        let d=null;
-        if(this.numWrap[0].innerHTML==''){
-            d=0;
-        }else if(this.numWrap[0].innerHTML!='' && this.numWrap[1].innerHTML==''){
-            d=1;
+    //从数组中移除指定值
+    removeLuckyNum(num){
+        // 数组扩展方法 从数组删除指定元素
+        Array.prototype.removeByValue = function(val) {
+            for (var i = 0; i < this.length; i++) {
+                if (this[i] == val) {
+                    this.splice(i, 1);
+                    break;
+                }
+            }
         }
-        return d;
+        this.allList.removeByValue(num);
     }
 
     // 开始滚动号码效果
     run(){
-
-
-        // console.log('run：'+this._d)
-        let oNode=document.createElement('span');
         clearInterval(this.timer);//清除之前的定时器
-        this.timer=setInterval(()=>{
-            // console.log(Math.floor(Math.random()*10));
-            // console.log(d)
-            // this.numWrap[this._d].innerHTML=Math.floor(Math.random()*10);
-            this.numPwrap.innerHTML='<span class="num">'+Math.floor(Math.random()*10)+'</span>'
-        },50)
+        console.log('this.numWrap子节点个数：'+this.numWrap.length);
+        let oNode=document.createElement('b');
+
+
         this.playRuningMusic();//同时播放音效
+
+
+
+        if(this.numWrap.length==0 || this.numWrap.length==this.digits){//当此没有子节点或者子节点已经全部显示 的时候才进行抽取
+
+            this.numPwrap.innerHTML='';
+            this.d=0;
+            console.log('新')
+            this.numPwrap.appendChild(oNode);
+
+            this.timer=setInterval(()=>{
+                oNode.innerHTML=Math.floor(Math.random()*10); //号码不断滚动
+            },50)
+
+            this.luckyNum=this.getLucky();//
+            this.removeLuckyNum(this.luckyNum);//移除已经中奖的号码
+            console.log('allList：'+this.allList)
+
+            this.saveLuckyNum(this.luckyNum);//存储
+
+
+        }else{
+
+            this.numPwrap.appendChild(oNode);
+
+            this.timer=setInterval(()=>{
+                oNode.innerHTML=Math.floor(Math.random()*10); //号码不断滚动
+            },50)
+
+            this.luckyNum=this.luckyNum;
+            // console.log('中奖号码 local：'+this.luckyNum);
+            // this.numPwrap.innerHTML='';
+        }
+
+
     }
 
     // 停止号码滚动
     stop(){
+        // 移除b节点
+        let b=this.numPwrap.getElementsByTagName('b')[0];
+        if(b){
 
-        // console.log('stop：'+this._d)
-        // 先清空
-        for(let i=0;i<this.numWrap.length;i++){
-            // this.numWrap[i].innerHTML='';
-            console.log(this.numWrap[i])
+            this.numPwrap.removeChild(b);
+        }
 
-        }
-        // alert('stop')
-        // console.log(this.numPwrap.innerHTML)
-        if(!this.getLocalStorage(this.turn)){
-            this.luckyNum=this.getLucky();//当此轮没有抽取过 的时候才进行抽取
-            console.log('中奖号码：'+this.luckyNum);
-        }else{
-            return false;
-        }
-        // console.log('d:'+this._d);//1
         clearInterval(this.timer);
         this.playStopMusic();
         let aLuckyNum=this.luckyNum.split('');//将中奖号码进行分割成数组
 
 
-        // this.numWrap[this._d].innerHTML=aLuckyNum[this._d];//显示
-        // 显示中奖号码
-        let d=null;
-        if(this.numWrap[0].innerHTML==''){
-            d=0;
-            this.numPwrap.innerHTML=aLuckyNum[0];
-        }else if(this.numWrap[0].innerHTML!='' && this.numWrap[1].innerHTML==''){
-            this.numPwrap.innerHTML=aLuckyNum[1];
-        }
+        if(this.numWrap.length<this.digits){
 
-        // console.log(str)
-        // this.saveLuckyNum();//存储
-        // if()
+            this.numPwrap.innerHTML+='<span class="num">'+aLuckyNum[this.d]+'</span>';
+        }else{
+
+        }
+        this.d++;
+        console.log('d：'+this.d)
+        /*if(this.d<this.digits-1){
+            this.d++;
+        }else{
+            this.d=this.digits-1;
+        }*/
     }
 
     // 滚动音效
@@ -160,27 +204,39 @@ class Lucky{
     }
 
     //将中奖号码进行存储
-    saveLuckyNum(){
-        if(!oLocalStorage[this.turn]){//如果次轮抽奖结果没有存储就进行存储
-            this.oLocalStorage.setItem(this.turn,this.getLucky());
+    saveLuckyNum(num){
+        if(!this.oLocalStorage[this.turn]){//如果次轮抽奖结果没有存储就进行存储
+            this.oLocalStorage.setItem(this.turn,num);
         }else{
-            this.oLocalStorage.setItem('new-'+this.turn,this.getLucky());
+            this.oLocalStorage.setItem(this.turn,this.getLocalStorage(this.turn)+','+num);
         }
-        // console.log(this.oLocalStorage)
     }
 
     getLocalStorage(turn){
         if(this.oLocalStorage[turn]){
-            // return
-            this.numWrap.innerHTML=this.oLocalStorage[turn];
+            return this.oLocalStorage[turn];
         }else{
-            // this.numWrap.innerHTML='';
+            return '';
         }
     }
+
+    // 将中奖号码填充到页面
+    fill(){
+        this.turnsWrap.innerHTML='第'+this.turn+'轮';
+        this.numPwrap.innerHTML='';
+        this.d=0;
+        if(this.getLocalStorage(this.turn) && this.numWrap.length==0){
+            this.numPwrap.innerHTML='';
+            let aLuckyNum=this.getLocalStorage(this.turn).split('');//将中奖号码进行分割成数组
+            for(let i=0 ;i<aLuckyNum.length;i++){
+                this.numPwrap.innerHTML='<span>'++'</span>'+this.getLocalStorage(this.turn);
+            }
+        }else{
+
+        }
+
+    }
 }
-
-
-
 
 
 
@@ -194,17 +250,9 @@ window.onload=function(){
         alert('您的浏览器不支持window.localStorage，请更换');
         return false;
     }
-    // 数组扩展方法 从数组删除指定元素
-    Array.prototype.removeByValue = function(val) {
-        for (var i = 0; i < this.length; i++) {
-            if (this[i] == val) {
-                this.splice(i, 1);
-                break;
-            }
-        }
-    }
 
-    let lucky=new Lucky(1,40,2,'get_num');
+
+    let lucky=new Lucky(1,90,'get_num',8);
     lucky.init();
 
 }
