94a63a933a56c7a44960202f42f5bd50ec6a0b1d
LuvTian <565195693@qq.com>
2021-01-07 23:24:54 +0800

    ok

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
-const { createApp, reactive,toRefs, onMounted, onUnmounted} = Vue
+const { createApp, ref, reactive, toRefs, computed, onMounted, onUnmounted} = Vue
 const App = {
     setup() {
         
         // console.log('setup');
-        const {lucky} = useStart()
+        const {lucky, newInitNum, config, rollingN} = useStart()
         return {
-            lucky
+            lucky,
+            newInitNum,
+            config,
+            rollingN
         }
     }
 }
@@ -16,18 +19,26 @@ function useShuffle(array) {
     if (!length) {
         return []
     }
+    const maxL = (Math.max(...array)+'').length
     let index = -1
     const lastIndex = length - 1
     const [...newArr] = array //拷贝数组
     while (++index < length) {
         const rand = index + Math.floor(Math.random() * (lastIndex - index + 1))
         const value = newArr[rand]
-        newArr[rand] = newArr[index]
-        newArr[index] = value
+        newArr[rand] = numPadStart(maxL,newArr[index])
+        newArr[index] = numPadStart(maxL,value)
     }
+    
     return newArr
 }
-
+// 数字补位
+function numPadStart(targetLength,num){
+    if(num){
+        return (num+'').padStart(targetLength,'0')
+    }
+    return num
+}
 /**
  * 从数组截取元素
  * @param {arr} 参与的号码
@@ -53,7 +64,7 @@ function useSwitchPlayBgm(audio){
         },200);
     }else{
         var t = setInterval(function(){
-            v-=0.12;
+            v-=0.06;
             // console.log('v2:'+v);
             if(v>0){
                 audio.volume = v;
@@ -66,39 +77,53 @@ function useSwitchPlayBgm(audio){
     
 }
 
+function useNumRolling(arr){
+    // const randomN = ref(null)
+    let randomIndex=Math.floor(Math.random()*arr.length);
+    randomN = arr[randomIndex]
+    return randomN
+}
 
 function useStart(){
-    const defaultArr = [1, 2, 4, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 20, 23,31,41,63,3]
-    // const defaultArr = [1, 2, 4, 6,8,15,42]
+    const initNum = [1, 2, 4, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 20, 23,31,41,63,55,56,57,62,61,63,64,65]
+    // const initNum = [1, 2, 4, 6,8,15,42]
     const config= [
         {
             'round': 1, //轮次
-            'm': 600, //金额
             'title': '600元代金券', //标题
-            'count': 2, //抽取个数
-            'self': [5] //内
+            'count': 10, //抽取个数
+            'self': ['05'] //内号一定要补全0
         },
         {
             'round': 2, //轮次
-            'm': 1000, //金额
             'title': '1000元代金券', //标题
             'count': 3, //抽取个数
-            'self': [4, 2] //内
+            'self': ['04', '22'] //内
+        },
+        {
+            'round': 3, //轮次
+            'title': '半价种植牙代金券', //标题
+            'count': 2, //抽取个数
+            'self': [] //内
+        },
+        {
+            'round': 4, //轮次
+            'title': '免费种植牙代金券', //标题
+            'count': 1, //抽取个数
+            'self': ['41'] //内
         }
     ]
 
-    const startAudio = new Audio()
-    startAudio.src = './assets/music/run.mp3'
-    startAudio.loop = true
-    startAudio.volume = 0
+    const rollingAudio = new Audio()
+    rollingAudio.src = './assets/music/run.mp3'
+    rollingAudio.loop = true
+    rollingAudio.volume = 0
     const stopAudio = new Audio()
     stopAudio.src = './assets/music/stop.mp3'
 
     let state = true // 开始与停止标记
-
-    const result = reactive({
-        lucky: []
-    })
+    let timer = null
+    
 
     // let result = ref([])
     onMounted(() => {
@@ -109,47 +134,90 @@ function useStart(){
     })
     // console.log(e);
 
-    let newArr = useShuffle(defaultArr) //参与抽奖的号码-乱序
+    let newArr = useShuffle(initNum) //参与抽奖的号码-乱序
+
+    const result = reactive({
+        lucky: [],
+        newInitNum: newArr,
+        config,
+        rollingN: Array(config.length)
+    })
+
     let allSelf = [] //提取所有内号
     for (let item of config) {
         allSelf = allSelf.concat(item.self)
     }
-    let joinNum = []; //剔除内号 得到真实参与抽奖的号码
-    newArr.forEach( v => {
-        if(allSelf.indexOf(v) === -1) joinNum.push(v)
-    })
+    let joinNum = window.localStorage.getItem('joinNum') ? JSON.parse(window.localStorage.getItem('joinNum')) : []; //剔除内号 得到真实参与抽奖的号码
+
+    if(!window.localStorage.getItem('joinNum')){
+        newArr.forEach( v => {
+            if(allSelf.indexOf(v) === -1) joinNum.push(v)
+        })
+    }
+    
+    
 
     function luckyDraw(e){
         if(e.keyCode == 32 && state){ //按下空格键 开始抽奖
+            if(joinNum.length<config[0].count){  //检测号码是否够用
+                alert('已经抽完了') 
+                return false
+            }
+            console.log('开始',joinNum);
             state = false
-            useSwitchPlayBgm(startAudio)
+            useSwitchPlayBgm(rollingAudio)
+            clearInterval(timer)
+            // let i = 0
             
-        }else if(e.keyCode == 32 && !state){
+            timer = setInterval(()=>{
+                for (let i = 0; i < result.rollingN.length; i++) {
+                    result.rollingN[i] = useNumRolling(newArr)
+                }
+                // 不用循环的方式，滚动有点慢
+                /* if(i>=result.rollingN.length){
+                    i = 0
+                }
+                result.rollingN[i] = useNumRolling(newArr)
+                i++ */
+            },50)
+            
+        }else if(e.keyCode == 32 && !state){ //按空格键停止
             state = true
-            useSwitchPlayBgm(startAudio)
-
+            useSwitchPlayBgm(rollingAudio)
+            result.rollingN =  Array(config.length)
             let i = 0
-            let timer
+            // let timer
             clearInterval(timer)
             timer = setInterval(() => {
-                if(i === config.length-1){
+                if(i === config.length){
                     clearInterval(timer)
                     useSwitchPlayBgm(stopAudio)
+                    window.localStorage.setItem('flag','over')
+                    console.log('结束',joinNum);
+                    window.localStorage.setItem('joinNum',JSON.stringify(joinNum))
+                    return false
                 }
                 result.lucky.push({
                     title: config[i].title,
-                    lucky: useShuffle([...config[i].self,...useDraw(joinNum,config[i].count - config[i].self.length)])
+                    lucky: window.localStorage.getItem('flag')=='over' ? [...useDraw(joinNum,config[i].count)] : useShuffle([...config[i].self,...useDraw(joinNum,config[i].count - config[i].self.length)])
                 })
                 i++
+                // console.log('?',joinNum);
             }, 2000);
             
             
-            
+        }
+
+        switch (e.keyCode || e.ctrlKey) {
+            case 90 || true:
+                window.localStorage.clear();
+                break;
+            default:
+                break;
         }
     }
-    // console.log(result);
-    return toRefs(result)
     
+    return toRefs(result)
 }
 
 
