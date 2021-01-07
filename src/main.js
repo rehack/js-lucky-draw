5afc3fb1fb33ec4dd487562b0311c7cfa04b03d4
codesboy <565195693@qq.com>
2021-01-07 14:06:32 +0800

    update

--- src/main.js
+++ src/main.js
@@ -37,6 +37,36 @@ function useDraw(arr,count){
     return arr.splice(0,count)
 }
 
+// 背景音乐淡入淡出切换
+function useSwitchPlayBgm(audio){
+    let v = audio.volume;
+    if (audio.paused) {
+        audio.play()
+        var t = setInterval(function(){
+            v+= 0.1;
+            // console.log('v1:'+v);
+            if(v<=1){
+                audio.volume = v;
+            }else{
+                clearInterval(t);
+            }
+        },200);
+    }else{
+        var t = setInterval(function(){
+            v-=0.12;
+            // console.log('v2:'+v);
+            if(v>0){
+                audio.volume = v;
+            }else{
+                clearInterval(t);
+                audio.pause();
+            }
+        },500);
+    }
+    
+}
+
+
 function useStart(){
     const defaultArr = [1, 2, 4, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 20, 23,31,41,63,3]
     // const defaultArr = [1, 2, 4, 6,8,15,42]
@@ -57,7 +87,14 @@ function useStart(){
         }
     ]
 
-    
+    const startAudio = new Audio()
+    startAudio.src = './assets/music/run.mp3'
+    startAudio.loop = true
+    startAudio.volume = 0
+    const stopAudio = new Audio()
+    stopAudio.src = './assets/music/stop.mp3'
+
+    let state = true // 开始与停止标记
 
     const result = reactive({
         lucky: []
@@ -72,28 +109,42 @@ function useStart(){
     })
     // console.log(e);
 
+    let newArr = useShuffle(defaultArr) //参与抽奖的号码-乱序
+    let allSelf = [] //提取所有内号
+    for (let item of config) {
+        allSelf = allSelf.concat(item.self)
+    }
+    let joinNum = []; //剔除内号 得到真实参与抽奖的号码
+    newArr.forEach( v => {
+        if(allSelf.indexOf(v) === -1) joinNum.push(v)
+    })
+
     function luckyDraw(e){
-        if(e.keyCode == 32){ //按下空格键 开始抽奖
-            let newArr = useShuffle(defaultArr) //参与抽奖的号码-乱序
-            let allSelf = [] //所有内号
-            for (let item of config) {
-                allSelf = allSelf.concat(item.self)
-            }
-            let joinNum = []; //剔除内号 得到真实参与抽奖的号码
-            newArr.forEach( v => {
-                if(allSelf.indexOf(v) === -1) joinNum.push(v)
-            })
-    
-            config.map(i => {
-                result.lucky.push({
-                    title: i.title,
-                    lucky: useShuffle([...i.self,...useDraw(joinNum,i.count - i.self.length)])
-                })
-            })
-    
+        if(e.keyCode == 32 && state){ //按下空格键 开始抽奖
+            state = false
+            useSwitchPlayBgm(startAudio)
             
-        }else{
-            // alert(e.keyCode)
+        }else if(e.keyCode == 32 && !state){
+            state = true
+            useSwitchPlayBgm(startAudio)
+
+            let i = 0
+            let timer
+            clearInterval(timer)
+            timer = setInterval(() => {
+                if(i === config.length-1){
+                    clearInterval(timer)
+                    useSwitchPlayBgm(stopAudio)
+                }
+                result.lucky.push({
+                    title: config[i].title,
+                    lucky: useShuffle([...config[i].self,...useDraw(joinNum,config[i].count - config[i].self.length)])
+                })
+                i++
+            }, 2000);
+            
+            
+            
         }
     }
     // console.log(result);
